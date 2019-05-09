c9dd54dd3451a97c78af84bb6c69a9342a1f8275
Ashish Salaskar <ashishsalaskar1@gmail.com>
2019-05-09 21:13:35 +0530

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,100 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this -> setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit -> setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,"Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open((QIODevice::ReadOnly | QFile::Text))){
+        QMessageBox::warning(this,"Warning","Cannot open file; "+ file.errorString());
+        return;
+    }
+    else{
+        QMessageBox::warning(this,"Warning","File opened succesfully");
+    }
+
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_As_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,"Save as");
+    QFile file(fileName);
+    currentFile = fileName;
+    if(!file.open((QIODevice::WriteOnly | QFile::Text))){
+        QMessageBox::warning(this,"Warning","Cannot Save file; "+ file.errorString());
+        return;
+    }
+    else{
+        QMessageBox::warning(this,"Warning","File saved succesfully");
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+    QPrinter printer;
+    printer.setPrinterName("Printer name");
+    QPrintDialog pDialog(&printer,this);
+
+    if(!(pDialog.exec() == QDialog::Rejected)){
+        QMessageBox::warning(this,"Warning","Cannot Access Printer");
+        return;
+    }
+
+    ui -> textEdit -> print(&printer);
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui -> textEdit -> copy();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+     ui -> textEdit -> paste();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+     ui -> textEdit -> redo();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+     ui -> textEdit -> undo();
+}
